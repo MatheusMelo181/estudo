77448e37eee7e0eb55f42d7d77d3881dd63c9eaa
Matheus Rocha <matheusmelodarocha18@gmail.com>
2022-06-28 14:59:06 -0300

    Criação de novo projeto + injeção de dependências

---
+++ vendas/src/main/java/com/loja/vendas/model/ClienteModel.java
@@ -0,0 +1,13 @@
+package com.loja.vendas.model;
+
+public class ClienteModel {
+
+    private String Nome;
+
+    public String getNome() {
+        return Nome;
+    }
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+}
---
+++ vendas/src/main/java/com/loja/vendas/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.loja.vendas.repository;
+
+import com.loja.vendas.model.ClienteModel;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClienteRepository {
+    public void persistir(ClienteModel clienteModel) {
+
+    }
+}
